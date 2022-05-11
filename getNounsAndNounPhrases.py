fd6bb2a712a4c7fcc9c5de398f06c653bb4f85d9
Yuki Nagato <37764578+YUKI2eN3e@users.noreply.github.com>
2022-05-11 15:02:13 -0700

    Got docx working

--- getNounsAndNounPhrases.py
+++ getNounsAndNounPhrases.py
@@ -4,14 +4,18 @@ from sys import argv
 import docx
 
 def getText(filename):
-    doc = docx.Document(filename)
-    fullText = []
-    for para in doc.paragraphs:
-        fullText.append(para.text)
-    return '\n'.join(fullText)
+	doc = docx.Document(filename)
+	fullText = []
+	for para in doc.paragraphs:
+		fullText.append(para.text)
+	for table in doc.tables:
+		for row in table.rows:
+			for cel in row.cells:
+				fullText.append(cel.text)
+	return '\n'.join(fullText)
 
 EXCLUTIONS = ["preconditions", "post-conditions", "scenario", "actor", "yyyy/mm/dd", "extensions", "merges", "step",
-	"case document name", "alternative path" "ui actions", "primary actor", "customer stakeholders", "main success",
+	"case document name", "alternative path", "ui actions", "primary actor", "customer stakeholders", "main success",
 	"basic flow", "business layer", "system response", "case diagram", "main scenario", "alternative flows", 
 	"alternative path", "scenario branches"]
 
@@ -32,12 +36,12 @@ if len(argv) > 1:
 	i = 1
 	print("Nouns:")
 	for noun in remove_duplicates(blob.noun_phrases):
-		if (' ' not in noun) and (noun not in EXCLUTIONS):
+		if (' ' not in noun) and (noun.strip().lower() not in EXCLUTIONS):
 			print("\t%d: %s" % (i,noun))
 			i = i + 1
 	print("Noun Phrases:")
 	for nounP in remove_duplicates(blob.noun_phrases):
-		if (' ' in nounP) and (nounP not in EXCLUTIONS):
+		if (' ' in nounP) and (nounP.strip().lower() not in EXCLUTIONS):
 			print("\t%d: %s" % (i,nounP))
 			i = i + 1
 else:
