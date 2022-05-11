e38395bc200af0ccb42e9a1cfadf6dd09fc0f8ea
Yuki Nagato <37764578+YUKI2eN3e@users.noreply.github.com>
2022-05-11 14:03:10 -0700

    Added exclutions

--- getNounsAndNounPhrases.py
+++ getNounsAndNounPhrases.py
@@ -2,6 +2,11 @@
 from textblob import TextBlob
 from sys import argv
 
+EXCLUTIONS = ["preconditions", "post-conditions", "scenario", "actor", "yyyy/mm/dd", "extensions", "merges", "step",
+	"case document name", "alternative path" "ui actions", "primary actor", "customer stakeholders", "main success",
+	"basic flow", "business layer", "system response", "case diagram", "main scenario", "alternative flows", 
+	"alternative path", "scenario branches"]
+
 def remove_duplicates(words):
 	return list(dict.fromkeys(words))
 
@@ -15,12 +20,12 @@ if len(argv) > 1:
 	i = 1
 	print("Nouns:")
 	for noun in remove_duplicates(blob.noun_phrases):
-		if ' ' not in noun:
+		if (' ' not in noun) and (noun not in EXCLUTIONS):
 			print("\t%d: %s" % (i,noun))
 			i = i + 1
 	print("Noun Phrases:")
 	for nounP in remove_duplicates(blob.noun_phrases):
-		if ' ' in nounP:
+		if (' ' in nounP) and (nounP not in EXCLUTIONS):
 			print("\t%d: %s" % (i,nounP))
 			i = i + 1
 else:
